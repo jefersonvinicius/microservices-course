99dc62b35d22782aaea00265bc6f33c00159eb1f
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-02-01 20:33:26 -0300

    Add .env load

--- DevOps/CD/challenge/src/main.go
+++ DevOps/CD/challenge/src/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"./calculator"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load(".env")
+
 	fmt.Printf("5 + 5 = %d\n", calculator.Sum(5, 5))
+	fmt.Printf("CONFIG VALUE: %s\n", os.Getenv("MY_EXAMPLE_CONFIG_FIELD"))
 }
