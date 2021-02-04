592d4bc208e00a5970cc9681c9e51770b5021f68
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-02-04 20:35:03 -0300

    Pushing image with cloudbuild.prod

--- DevOps/CD/challenge/src/main.go
+++ DevOps/CD/challenge/src/main.go
@@ -12,5 +12,14 @@ func main() {
 	godotenv.Load(".env")
 
 	fmt.Printf("5 + 5 = %d\n", calculator.Sum(5, 5))
+
+	var secretvalue string
+	if len(os.Getenv("SECRET_VALUE")) > 0 {
+		secretvalue = os.Getenv("SECRET_VALUE")
+	} else {
+		secretvalue = "Não configurado"
+	}
+
 	fmt.Printf("CONFIG VALUE: %s\n", os.Getenv("MY_EXAMPLE_CONFIG_FIELD"))
+	fmt.Printf("SECRET VALUE: %s\n", secretvalue)
 }
